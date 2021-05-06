b85b3fc00c628b583be84d6366f4eb5c2ac95ba3
QALatvia <qalatvia@itunes.com>
2021-05-06 10:50:24 +0300

    validation and random added

--- cypress/integration/my-own-tests/postcode.test.js
+++ cypress/integration/my-own-tests/postcode.test.js
@@ -38,19 +38,15 @@ describe('First Test', () =>{
         })
 
 
-        //Date and time
-        //cy.get('[class="day-component available-day "]').contains('1').click()
-        //cy.get('[class="available"]').contains('15:00').click()
-
         //Input form
-        // cy.get('[id="val_vend_full_name"]').type('Full Name')
-        // cy.get('[id="val_vend_email"]').type('Email@email')
-        // cy.get('[id="val_vend_phone"]').type('123456789')
-        //
-        // cy.get('[id="val_opt_in"]').click()
-        // cy.get('[id="val_vend_full_name_error"]').should('not.exist')
-        // cy.get('[id="val_vend_email_error"]').should('be.visible')
-        // cy.get('[id="val_vend_phone_error"]').should('be.visible')
+        cy.get('[id="val_vend_full_name"]').type('Full Name')
+        cy.get('[id="val_vend_email"]').type('Email@email')
+        cy.get('[id="val_vend_phone"]').type('123456789')
+        cy.get('[id="val_opt_in"]').click()
+
+        cy.get('[id="val_vend_full_name_error"]').should('not.exist')
+        cy.get('[id="val_vend_email_error"]').should('be.visible')
+        cy.get('[id="val_vend_phone_error"]').should('be.visible')
 
 
     })
