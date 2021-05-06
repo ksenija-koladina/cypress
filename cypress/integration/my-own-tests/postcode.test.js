9ef1c0677ef8c205bd0082fc15fee212325b175f
QALatvia <qalatvia@itunes.com>
2021-05-06 10:25:58 +0300

    random select added

--- cypress/integration/my-own-tests/postcode.test.js
+++ cypress/integration/my-own-tests/postcode.test.js
@@ -13,22 +13,44 @@ describe('First Test', () =>{
         cy.contains('Select Your Address').should('exist')
 
         //Address
-        cy.get('.list-group-item').should('contain.text', 'Flat ')
-        cy.contains('Flat').click()
+
+        cy.document().then((doc) => {
+            const flats = doc.querySelectorAll(".list-group-item")
+            let count = flats.length;
+            let random = Math.floor(Math.random() * count);
+            cy.get(flats[random]).click();
+        })
+            //Date and time
+        cy.document().then((doc) => {
+            const days = doc.querySelectorAll(".day-component.available-day")
+            let count = days.length;
+            let random = Math.floor(Math.random() * count);
+            cy.get(days[random]).click();
+
+        })
+
+        cy.document().then((doc) => {
+
+            const timeAv = doc.querySelectorAll(".timeslot > .available")
+            let count = timeAv.length;
+            let random = Math.floor(Math.random() * count);
+            cy.get(timeAv[random]).click();
+        })
+
 
         //Date and time
-        cy.get('[class="day-component available-day "]').contains('1').click()
-        cy.get('[class="available"]').contains('15:00').click()
+        //cy.get('[class="day-component available-day "]').contains('1').click()
+        //cy.get('[class="available"]').contains('15:00').click()
 
         //Input form
-        cy.get('[id="val_vend_full_name"]').type('Full Name')
-        cy.get('[id="val_vend_email"]').type('Email@email')
-        cy.get('[id="val_vend_phone"]').type('123456789')
-
-        cy.get('[id="val_opt_in"]').click()
-        cy.get('[id="val_vend_full_name_error"]').should('not.exist')
-        cy.get('[id="val_vend_email_error"]').should('be.visible')
-        cy.get('[id="val_vend_phone_error"]').should('be.visible')
+        // cy.get('[id="val_vend_full_name"]').type('Full Name')
+        // cy.get('[id="val_vend_email"]').type('Email@email')
+        // cy.get('[id="val_vend_phone"]').type('123456789')
+        //
+        // cy.get('[id="val_opt_in"]').click()
+        // cy.get('[id="val_vend_full_name_error"]').should('not.exist')
+        // cy.get('[id="val_vend_email_error"]').should('be.visible')
+        // cy.get('[id="val_vend_phone_error"]').should('be.visible')
 
 
     })
